d58ab1416b406769c4486de8edf5894107dab430
alixcja <alicja.pater.2004@gmail.com>
2024-06-13 01:53:55 +0200

    🐛 Add booking page properly 'cuz I was dumb

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <v-app>
     <HomeHeader></HomeHeader>
+    <v-main class="main">
+      <BookingPage></BookingPage>
+    </v-main>
   </v-app>
 </template>
 <script setup lang="ts">
 import HomeHeader from "./ui/components/base/HomeHeader.vue";
+import BookingPage from "./ui/pages/BookingPage.vue";
 </script>
 <style scoped>
 .main {
