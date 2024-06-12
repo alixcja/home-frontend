a01e52aaabe4023292ee4a289e7b1669fa2f6eb4
alixcja <alicja.pater.2004@gmail.com>
2024-06-13 01:48:32 +0200

    🎉 half inital vue project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url';
+
+// https://vitejs.dev/config/
+export default ({ mode }: { mode: any }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+  },
+    server: {
+      port: parseInt(`${process.env.VITE_PORT}`),
+    },
+  });
+};
