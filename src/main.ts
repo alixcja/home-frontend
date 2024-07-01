c538fd4ca18c307872cfd0c63e9ac53336e121c3
alixcja <alicja.pater.2004@gmail.com>
2024-07-01 20:42:11 +0200

    🔥 Remove router  because I am apparently to stupid for that

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
 import { createPinia } from 'pinia'
-import { router } from '../src/router/index'
-
 
 const vuetify = createVuetify({
     components,
@@ -17,4 +15,4 @@ const vuetify = createVuetify({
   
 const pinia = createPinia()
 
-createApp(App).use(vuetify).use(pinia).use(router).mount('#app')
+createApp(App).use(vuetify).use(pinia).mount('#app')
