c5563dca5a9e15ba4baf61a6ef2fbd7809b33089
alixcja <alicja.pater.2004@gmail.com>
2025-05-22 22:52:04 +0200

    🚚 Rename AllShopsOverview to ShopsOverview

--- src/ui/pages/ShopPage.vue
+++ src/ui/pages/ShopPage.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="position-relative overflow-y-auto ma-6">
     <h1 class="header">Restaurants</h1>
-      <AllShopsOverview />
+      <ShopsOverview />
   </div>
 </template>
 <script lang="ts" setup>
 import { useShopStore } from "@/data/store/ShopStore";
 import { onMounted } from "vue";
-import AllShopsOverview from "@/ui/components/shop/AllShopsOverview.vue";
+import ShopsOverview from "@/ui/components/shop/ShopsOverview.vue";
 
 onMounted(() => {
   useShopStore().getAllShops();
